ecd90e40e52ee7b6d658b8702238bee7d8b9acdb
Parakh <parakhjain2301@gmail.com>
2023-06-14 16:47:50 +0530

    channel card is not re rendering again final 2

--- src/screens/channelsScreen/components/RecentChannelsList.js
+++ src/screens/channelsScreen/components/RecentChannelsList.js
@@ -1,40 +1,14 @@
 import React, {useMemo, useState} from 'react';
 import {Animated, FlatList} from 'react-native';
 import {useCallback} from 'react';
-import {RenderChannels, RenderSearchChannels} from '../ChannelCard';
+import {RenderChannels} from '../ChannelCard';
 import {View} from 'react-native';
 import {RefreshControl} from 'react-native';
 import {connect} from 'react-redux';
-import {getChannelsStart} from '../../../redux/actions/channels/ChannelsAction';
-import {getAllUsersOfOrgStart} from '../../../redux/actions/org/GetAllUsersOfOrg';
 
 const RecentChannelsListComponent = React.memo(
-  ({
-    channelsState,
-    onScroll,
-    orgsState,
-    userInfoState,
-    getChannelsAction,
-    getAllUsersOfOrgAction,
-  }) => {
-    const [refreshing, setRefreshing] = useState(false);
-
-    const onRefresh = useCallback(async () => {
-      setRefreshing(true);
-      await getChannelsAction(
-        userInfoState?.accessToken,
-        orgsState?.currentOrgId,
-        userInfoState?.user?.id,
-        userInfoState?.user?.displayName
-          ? userInfoState?.user?.displayName
-          : userInfoState?.user?.firstName,
-      );
-      await getAllUsersOfOrgAction(
-        userInfoState?.accessToken,
-        orgsState?.currentOrgId,
-      );
-      setRefreshing(false);
-    }, [orgsState?.currentOrgId]);
+  ({channelsState, onScroll, refreshing, onRefresh}) => {
+    console.log('recent channelt list');
 
     const memoizedData = useMemo(
       () => channelsState?.recentChannels || channelsState?.channels || [],
@@ -43,9 +17,20 @@ const RecentChannelsListComponent = React.memo(
 
     const renderItemChannels = useCallback(
       ({item, index}) => {
-        return !item?.isArchived && <RenderChannels item={item} />;
+        console.log(index);
+        return (
+          !item?.isArchived && (
+            <RenderChannels item={item} channelsState={channelsState} />
+          )
+        );
       },
-      [channelsState?.recentChannels],
+      [
+        channelsState?.recentChannels,
+        channelsState?.channels,
+        channelsState?.teamIdAndUnreadCountMapping,
+        channelsState?.teamIdAndBadgeCountMapping,
+        // channelsState?.highlightChannel,
+      ],
     );
     return (
       <View style={{flex: 1}}>
@@ -64,19 +49,10 @@ const RecentChannelsListComponent = React.memo(
   },
 );
 const mapStateToProps = state => ({
-  userInfoState: state.userInfoReducer,
   channelsState: state.channelsReducer,
-  orgsState: state.orgsReducer,
 });
-const mapDispatchToProps = dispatch => {
-  return {
-    getChannelsAction: (token, orgId, userId, userName) =>
-      dispatch(getChannelsStart(token, orgId, userId, userName)),
-    getAllUsersOfOrgAction: (accessToken, orgId) =>
-      dispatch(getAllUsersOfOrgStart(accessToken, orgId)),
-  };
-};
+
 export default RecentChannelsList = connect(
   mapStateToProps,
-  mapDispatchToProps,
+  null,
 )(RecentChannelsListComponent);
