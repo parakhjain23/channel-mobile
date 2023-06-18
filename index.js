23dd7d441142ac1652be14d57d786c158da178e8
Parakh <parakhjain2301@gmail.com>
2023-06-18 11:50:15 +0530

    integrate sentry and add push notification in xcode and added few key in info.plist

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppSentry from './AppSentry';
 
-AppRegistry.registerComponent(appName, () => App);
+if (!__DEV__) {
+  AppRegistry.registerComponent(appName, () => App);
+} else {
+  AppRegistry.registerComponent(appName, () => AppSentry);
+}
 // AsyncStorage.clear();
