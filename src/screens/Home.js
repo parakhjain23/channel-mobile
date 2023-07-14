fe271dd051134a48cb512b627db37a5bd346c441
Parakh <parakhjain2301@gmail.com>
2023-07-14 15:37:15 +0530

    implement divider textarea already selected checkbox

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,29 +2,40 @@ import {
   View,
   Text,
   useWindowDimensions,
-  Button,
   Linking,
   Image,
   TouchableOpacity,
   ScrollView,
   StyleSheet,
+  Button,
 } from 'react-native';
 import React, {useState} from 'react';
-import RenderHTML from 'react-native-render-html';
+import RenderHTML from 'react-native-render-html/src/RenderHTML';
 import {Checkbox, RadioButton, TextInput} from 'react-native-paper';
+import {
+  VictoryAxis,
+  VictoryBar,
+  VictoryChart,
+  VictoryLabel,
+  VictoryLine,
+  VictoryPie,
+  VictoryTheme,
+} from 'victory-native';
 
 let JSON = [
-  {type: 'Quill', value: '<h1>hello</h1>'},
+  {type: 'html', value: '<h1>hello</h1>'},
   {type: 'plain_text', value: 'hello'},
-  {type: 'Button', value: 'hello'},
+  {type: 'button', value: 'hello'},
+  {type: 'divider'},
+  {type: 'text_area'},
   {
-    type: 'Image',
+    type: 'image',
     url: 'https://photos.prnewswire.com/prnfull/20150402/10119680',
     width: 90,
     height: 50,
   },
   {
-    type: 'RadioButton',
+    type: 'radio_buttons',
     value: 'Protein',
     options: [
       {
@@ -39,7 +50,7 @@ let JSON = [
     action_id: 567,
   },
   {
-    type: 'Checkbox',
+    type: 'checkboxes',
     value: 'Protein',
     options: [
       {
@@ -65,7 +76,7 @@ let JSON = [
     action_id: 'form',
     elements: [
       {
-        type: 'Image',
+        type: 'image',
         url: 'https://photos.prnewswire.com/prnfull/20150402/10119680',
         width: 90,
       },
@@ -84,15 +95,15 @@ let JSON = [
         action_id: 'lname',
       },
       {
-        type: 'Section',
+        type: 'section',
         elements: [
           {
-            type: 'Checkbox',
-            value: 'Protein',
+            type: 'checkboxes',
             options: [
               {
                 type: 'plain_text',
-                value: 'first',
+                value: 'first hai',
+                selected: true,
               },
               {
                 type: 'plain_text',
@@ -104,41 +115,81 @@ let JSON = [
         ],
       },
       {
-        type: 'Button',
+        type: 'button',
         value: 'Submit',
         action_id: 'button',
       },
     ],
   },
-  // {
-  //   type: 'Card',
-  //   elements: [
-  //     {
-  //       type: 'Section',
-  //       elements: [
-  //         {
-  //           type: 'RadioButton',
-  //           value: 'Protein',
-  //           options: [
-  //             {
-  //               type: 'plain_text',
-  //               value: 'Protein',
-  //             },
-  //             {
-  //               type: 'plain_text',
-  //               value: 'Carbs',
-  //             },
-  //           ],
-  //           action_id: 567,
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       type: 'Button',
-  //       value: 'Submit',
-  //     },
-  //   ],
-  // },
+  {
+    type: 'card',
+    elements: [
+      {
+        type: 'section',
+        elements: [
+          {
+            type: 'radio_buttons',
+            value: 'Protein',
+            options: [
+              {
+                type: 'plain_text',
+                value: 'Protein',
+              },
+              {
+                type: 'plain_text',
+                value: 'Carbs',
+              },
+            ],
+            action_id: 567,
+          },
+        ],
+      },
+      {
+        type: 'button',
+        value: 'Submit',
+      },
+    ],
+  },
+  {
+    type: 'barGraph',
+    values: [
+      {x: 'MSG90', y: '5', label: '100cr'},
+      {x: 'MSG91', y: '15', label: '100cr'},
+      {x: 'MSG92', y: '5', label: '100cr'},
+      {x: 'MSG93', y: '25', label: '100cr'},
+      {x: 'MSG94', y: '10', label: '100cr'},
+      {x: 'MSG95', y: '5', label: '100cr'},
+      {x: 'MSG96', y: '5', label: '100cr'},
+      {x: 'MSG97', y: '5', label: '100cr'},
+    ],
+    categories: {
+      x: ['MSG90', 'MSG91', 'MSG92', 'MSG93', 'MSG94'],
+      y: ['5', '10', '15', '20', '25'],
+    },
+    xAxisLabel: 'revenue in Cr.',
+    yAxisLabel: 'time in Years',
+  },
+  {
+    type: 'pieChart',
+    values: [
+      {x: 'MSG91', y: 30},
+      {x: 'Gidh', y: 20},
+      {x: 'SPACE', y: 15},
+      {x: 'halfKg', y: 35},
+      {x: 'halfKg', y: 5},
+    ],
+    chatTitile: 'year 2023 contributions',
+  },
+  {
+    type: 'lineGraph',
+    values: [
+      {x: 1, y: 2},
+      {x: 2, y: 3},
+      {x: 3, y: 5},
+      {x: 4, y: 4},
+      {x: 5, y: 7},
+    ],
+  },
 ];
 
 const Home = () => {
@@ -146,7 +197,7 @@ const Home = () => {
   const [value, setValue] = React.useState({});
   console.log(data, '=-=-=');
 
-  const handleCheckboxToggle = (action_id, name) => {
+  const handleChekboxesToggle = (action_id, name) => {
     setData(prevData => ({
       ...prevData,
       [action_id]: {
@@ -156,7 +207,7 @@ const Home = () => {
     }));
   };
 
-  const handleRadioButtonToggle = (action_id, name) => {
+  const handleradio_buttonsToggle = (action_id, name) => {
     setValue(prevData => ({
       ...prevData,
       [action_id]: name,
@@ -172,7 +223,8 @@ const Home = () => {
   const renderComponent = (item, index) => {
     const {width} = useWindowDimensions();
     switch (item.type) {
-      case 'Quill':
+      case 'quill':
+      case 'html':
         return (
           <RenderHTML
             source={{
@@ -197,7 +249,7 @@ const Home = () => {
           />
         );
 
-      case 'Button':
+      case 'button':
         return (
           <View style={{flexShrink: 1}}>
             <Button
@@ -215,7 +267,7 @@ const Home = () => {
           </View>
         );
 
-      case 'Section':
+      case 'section':
         return (
           <View
             style={{
@@ -230,18 +282,22 @@ const Home = () => {
           </View>
         );
 
-      case 'Checkbox':
-        if (typeof data[item?.action_id] === 'undefined') {
-          setData(prevData => ({
-            ...prevData,
-            [item.action_id]: {},
-          }));
-        }
+      case 'checkboxes':
         return item?.options?.map((element, elementIndex) => {
+          if (typeof data[item?.action_id] === 'undefined') {
+            setData(prevData => ({
+              ...prevData,
+              [item.action_id]: {
+                ...prevData[item.action_id],
+                [element.value]: element.selected || false,
+              },
+            }));
+          }
+
           return (
             <TouchableOpacity
               onPress={() =>
-                handleCheckboxToggle(item?.action_id, element?.value)
+                handleChekboxesToggle(item?.action_id, element?.value)
               }
               style={{
                 flexDirection: 'row',
@@ -261,13 +317,12 @@ const Home = () => {
           );
         });
 
-      case 'RadioButton':
+      case 'radio_buttons':
         return (
           <RadioButton.Group
             onValueChange={value =>
-              handleRadioButtonToggle(item?.action_id, value)
+              handleradio_buttonsToggle(item?.action_id, value)
             }
-            // onValueChange={newValue => setValue(newValue)}
             value={value[item?.action_id]}>
             <View>
               {item?.options?.map(element => {
@@ -282,7 +337,7 @@ const Home = () => {
           </RadioButton.Group>
         );
 
-      case 'Image':
+      case 'image':
         return (
           <Image
             source={{
@@ -300,6 +355,26 @@ const Home = () => {
       case 'input':
         return (
           <TextInput
+            multiline={item?.multiline || false}
+            mode="outlined"
+            label={item?.label || ''}
+            placeholder={item?.placeholder || 'Input'}
+            onChangeText={text => onChange(item?.action_id, text)}
+          />
+        );
+
+      case 'text_area':
+        return (
+          <TextInput
+            multiline={true}
+            mode="outlined"
+            label={item?.label || ''}
+            placeholder={item?.placeholder || 'Write something'}
+            onChangeText={text => onChange(item?.action_id, text)}
+          />
+        );
+        return (
+          <TextInput
             mode="outlined"
             label={item?.label || ''}
             placeholder={item?.placeholder || ''}
@@ -307,7 +382,7 @@ const Home = () => {
           />
         );
 
-      case 'Card':
+      case 'card':
         return (
           <View style={styles.cardContainer}>
             {item?.elements?.map((element, index) =>
@@ -316,6 +391,80 @@ const Home = () => {
           </View>
         );
 
+      case 'divider':
+        return <View style={styles.divider} />;
+
+      case 'barGraph':
+        return (
+          <VictoryChart
+            theme={VictoryTheme.grayscale}
+            domainPadding={{x: 20, y: 20}}>
+            <VictoryLabel
+              text={`${item.xAxisLabel}`}
+              x={200}
+              y={290}
+              textAnchor="middle"
+              style={{fontWeight: '600'}}
+            />
+            <VictoryLabel
+              text={`${item.yAxisLabel}`}
+              x={10}
+              y={140}
+              textAnchor="middle"
+              verticalAnchor="middle"
+              angle={-90}
+              style={{fontWeight: '600'}}
+            />
+            <VictoryBar
+              categories={item.categories}
+              data={item.values}
+              style={{
+                labels: {
+                  fontSize: 14,
+                },
+              }}
+            />
+            <VictoryAxis
+              tickCount={data.length}
+              style={{
+                tickLabels: {
+                  fontSize: 8, // Adjust the font size as desired
+                  padding: 5, // Adjust the padding as desired
+                  angle: 20, // Adjust the rotation angle as desired
+                  textAnchor: 'start', // Adjust the text anchor as desired
+                },
+              }}
+            />
+            <VictoryAxis dependentAxis />
+          </VictoryChart>
+        );
+      case 'pieChart':
+        return (
+          <View>
+            <VictoryPie
+              data={item.values}
+              labels={({datum}) => `${datum.x}\n${datum.y}%`}
+            />
+            <Text
+              style={{fontSize: 14, fontWeight: '600', textAlign: 'center'}}>
+              {item.chatTitile}
+            </Text>
+          </View>
+        );
+      case 'lineGraph':
+        return (
+          <View style={{}}>
+            <VictoryChart theme={VictoryTheme.material}>
+              <VictoryLine
+                style={{
+                  data: {stroke: '#c43a31'},
+                  parent: {border: '1px solid #ccc'},
+                }}
+                data={item.values}
+              />
+            </VictoryChart>
+          </View>
+        );
       default:
         return null;
     }
@@ -347,5 +496,10 @@ const styles = StyleSheet.create({
       height: 3,
     },
     elevation: 5,
+  },
+  divider: {
+    height: 0.5,
+    backgroundColor: 'gray',
+    marginVertical: 10,
   },
 });
