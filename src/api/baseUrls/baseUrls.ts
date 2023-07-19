9a2c152527405449f6808fe29d232f927afe430d
Parakh <parakhjain2301@gmail.com>
2023-07-19 16:35:25 +0530

    baseurl.js to ts

--- src/api/baseUrls/baseUrls.ts
+++ src/api/baseUrls/baseUrls.ts
@@ -1,3 +1,3 @@
-export const CHAT_SERVER_URL = 'https://dev-api.intospace.io';
+export const CHAT_SERVER_URL = 'https://api.intospace.io';
 export const DELVE_SERVER_URL = 'https://delve-api.intospace.io';
 export const SOCKET_URL = 'wss://api.intospace.io';
