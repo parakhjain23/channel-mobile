f8e54880c98481f46d852ec1a36a2a9f49256f9c
Rudraksh <rudrakshkachhawa@gmail.com>
2023-09-24 01:12:46 +0530

    store app wraper

--- src/navigation/StoreAppWrapperV2.tsx
+++ src/navigation/StoreAppWrapperV2.tsx
@@ -1,11 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {initializeSocket} from '../redux/actions/socket/socketActions';
-// import SplashScreenComponent from '../screens/splashScreen/SplashScreen';
 import AuthNavigation from './AuthNavigation';
 import { useCustomSelector } from '../utils/deepCheckSelector';
 import { $ReduxCoreType } from '../types/reduxCoreType';
-// import { SplashScreenComponentV2 } from '../screens/splashScreen/SplashScreenV2';
+import { SplashScreenComponentV2 } from '../screens/splashScreen/SplashScreenV2';
 
 const StoreAppWrapperV2 = () => {
   const [showSplashScreen, setShowSplashScreen] = useState(true);
