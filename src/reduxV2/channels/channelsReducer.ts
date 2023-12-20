621f7968bccb3715879b1344f5a3ffbb1581ba53
Deepanshu Oswal <deepanshuoswal1989@gmail.com>
2023-12-20 12:08:23 +0530

    listin more events

--- src/reduxV2/channels/channelsReducer.ts
+++ src/reduxV2/channels/channelsReducer.ts
@@ -4,31 +4,31 @@ import { $ChannelsReduxType, channelDetailType } from '../../types/channelsReduc
 import { channelDataMappingUtility } from '../../utils/mappingUtility'
 export const initialState: $ChannelsReduxType = {
     isLoading: false,
-    channels: [], 
-    recentChannels: [], 
-    highlightChannel:{} ,
-    userIdAndTeamIdMapping:{} ,
-    teamIdAndDataMapping:{} ,
-    unReadAndBadgeCountMapping:{}
+    channels: [],
+    recentChannels: [],
+    highlightChannel: {},
+    userIdAndTeamIdMapping: {},
+    teamIdAndDataMapping: {},
+    unReadAndBadgeCountMapping: {}
 }
 
-export const reducers: ValidateSliceCaseReducers<$ChannelsReduxType, SliceCaseReducers<$ChannelsReduxType>>  = {
-    updateChannelState(state,action: actionType<any>){
-        
+export const reducers: ValidateSliceCaseReducers<$ChannelsReduxType, SliceCaseReducers<$ChannelsReduxType>> = {
+    updateChannelState(state, action: actionType<any>) {
+
     },
-    getAllChannelsSuccessV2(state,action:actionType<{channels: channelDetailType[] , userId:string,userName:string}>){
-        const {userIdAndTeamIdMapping , teamIdAndDataMapping} = channelDataMappingUtility(action.payload)
+    getAllChannelsSuccessV2(state, action: actionType<{ channels: channelDetailType[], userId: string, userName: string }>) {
+        const { userIdAndTeamIdMapping, teamIdAndDataMapping } = channelDataMappingUtility(action.payload)
         state.channels = action?.payload?.channels
         state.userIdAndTeamIdMapping = userIdAndTeamIdMapping
         state.teamIdAndDataMapping = teamIdAndDataMapping
     },
-    getAllRecentChannelSuccessV2(state,action:actionType<{recentChannels: channelDetailType[] , userId:string,userName:string}>){
+    getAllRecentChannelSuccessV2(state, action: actionType<{ recentChannels: channelDetailType[], userId: string, userName: string }>) {
         var tempRecentChannels = [];
         var key = null;
         for (let i = 0; i < action?.payload?.recentChannels?.length; i++) {
-          key = action?.payload?.recentChannels[i]?.teamId;
-          tempRecentChannels.push(state?.teamIdAndDataMapping[key]);
+            key = action?.payload?.recentChannels[i]?.teamId;
+            tempRecentChannels.push(state?.teamIdAndDataMapping[key]);
         }
-        return {...state, recentChannels: tempRecentChannels};
+        return { ...state, recentChannels: tempRecentChannels };
     }
 }
